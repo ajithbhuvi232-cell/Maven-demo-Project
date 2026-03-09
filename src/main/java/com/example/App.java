16ed30501ce8612a4fa3b45185b533791922d324
Ajith Kumar <ajith.kumar@wisseninfotech.com>
2026-03-09 19:36:11 +0530

    testing webhook

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Maven project!");
+        System.out.println("Hello from CI/CD pipeline test");
     }
 }
